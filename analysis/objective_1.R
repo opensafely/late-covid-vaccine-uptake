0610bf9c98e2d68fa7289e44c69a541da15a11df
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-05 08:58:25 +0100

    Create objective_1.R

---
+++ analysis/objective_1.R
@@ -0,0 +1,12 @@
+# TODO Femi
+
+# Complete the following two tasks, preparing datasets that are safe to be 
+# released from the server.
+# 1. Summarise the following variables in a table:
+#    age_2, sex, *_group, region, death_date
+# 2. Summarise the distribution of dates of first dose by jcvi group.
+
+# The following resources might be useful for making sure outputs are safe:
+# https://docs.opensafely.org/releasing-files/
+# The roundmid_any function in the following file:
+source(here::here("analysis", "functions", "utility.R"))
