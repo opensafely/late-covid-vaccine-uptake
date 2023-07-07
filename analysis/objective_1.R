7c235e1a8ba90703ac73aefd5f6cf7f172db6291
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-07 13:17:31 +0100

    Update objective_1.R

--- analysis/objective_1.R
+++ analysis/objective_1.R
@@ -3,10 +3,15 @@
 # Complete the following two tasks, preparing datasets that are safe to be 
 # released from the server.
 # 1. Summarise the following variables in a table:
-#    age_2, sex, *_group, region, death_date
-# 2. Summarise the distribution of dates of first dose by jcvi group.
+#    age_2, sex, *_group, region, death_date (range of dates, number of missing dates)
+# 2. Summarise the distribution of dates of first dose by jcvi group (covid_vax_disease_1_date)
 
 # The following resources might be useful for making sure outputs are safe:
 # https://docs.opensafely.org/releasing-files/
 # The roundmid_any function in the following file:
+library(tidyverse)
+library(here)
+
 source(here::here("analysis", "functions", "utility.R"))
+
+data_processed <- read_rds(here("output", "extract", "data_processed.rds"))
