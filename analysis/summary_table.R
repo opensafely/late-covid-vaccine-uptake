90ec4688d81678405ca582000283afc74d51c9bc
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-12 10:31:20 +0100

    Create summary_table.R

---
+++ analysis/summary_table.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(here)
+library(gtsummary)
+
+source(here::here("analysis", "functions", "utility.R"))
+
+data_processed <- read_rds(here("output", "extract", "data_processed.rds"))
+
+# create a summary table of age_2 and sex
+
+table1 <- data_processed %>%
+  tbl_summary(
+    # specify the columns to summarise
+    include = c(age_2, sex)
+  )
+
+# print table1 in viewer
+table1
+
+# see raw data for table1
+table1$meta_data %>%
+  select(var_label, df_stats) %>%
+  unnest(df_stats)
+
+# you can see here that the function gtsummary::tbl_summary() automatically 
+# detects the type of column, and calculates the relevant statistics
+
+# TODO
+# try adding more columns to the summary table
