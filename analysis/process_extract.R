f50abafec0a3f272c0c89015e2d67b4505f80b0b
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2023-07-17 20:16:46 +0100

    Update process_extract.R

--- analysis/process_extract.R
+++ analysis/process_extract.R
@@ -40,16 +40,85 @@ data_processed <- extract %>%
   # reorder so patient_id is first column
   select(patient_id, everything())
 
+# tidy up
+rm(extract)
+
 # TODO Elsie
 # write in checks to make sure at risk group, JCVI groups and elig dates have 
 # been derived correctly in study definition
 
-# TODO
-# As we add more variables we will need to add more processing steps to this script
-  
-# save the processed data
+# apply exclusions
+data_processed <- data_processed %>%
+  mutate(
+    # create variables for applying the eligibility criteria
+    aged_over_18 = age_2 >=18,
+    alive_on_elig_date = is.na(death_date) | elig_date <= death_date,
+    aged_under_120 = age_2 < 120,
+    no_vax_before_start = is.na(covid_vax_disease_1_date) | as.Date(study_parameters$start_date) <= covid_vax_disease_1_date,
+    sex_recorded = !is.na(sex),
+    region_recorded = !is.na(region),
+    imd_recorded = imd_Q5 != "Unknown",
+    ethnicity_recorded = ethnicity != "Unknown",
+    # apply the eligibility criteria
+    c0_descr = "All patients in OpenSAFELY-TPP",
+    c0 = TRUE,
+    c1_descr = "   aged 18 years or over",
+    c1 = c0 & aged_over_18,
+    c2_descr = "   alive on eligibility date",
+    c2 = c1 & alive_on_elig_date,
+    c3_descr = "   registered with one TPP general practice between 2020-01-01 and eligibility date",
+    c3 = c2 & has_follow_up,
+    c4_descr = "   aged under 120 years",
+    c4 = c3 & aged_under_120,
+    c5_descr = "   no vaccination before rollout",
+    c5 = c4 & no_vax_before_start,
+    c6_descr = "   sex, region, IMD and ethnicity recorded",
+    c6 = c5 & sex_recorded & region_recorded & imd_recorded & ethnicity_recorded,
+    include = c6
+  )
+
+# data for flowchart
+data_flowchart <- data_processed %>%
+  select(patient_id, matches("^c\\d+")) %>%
+  rename_at(vars(matches("^c\\d+$")), ~str_c(., "_value")) %>%
+  pivot_longer(
+    cols = matches("^c\\d+"),
+    names_to = c("crit", ".value"),
+    names_pattern = "(.*)_(.*)"
+  ) %>%
+  group_by(crit, descr) %>%
+  summarise(n = sum(value), .groups = "keep") %>%
+  ungroup() %>%
+  rename(criteria = descr) %>%
+  arrange(crit) 
+
+# save flowchart without rounding in case needed for debugging
+data_flowchart %>%
+  flow_stats_rounded(1) %>%
+  write_csv(file.path(dir_path, "flowchart_raw.csv"))
+
+# save flowchart with rounding for releasing
+data_flowchart %>%
+  flow_stats_rounded(threshold) %>%
+  write_csv(file.path(dir_path, glue("flowchart_midpoint{threshold}.csv")))
+
+# only keep the variables that are needed for the eligible patients 
+data_eligible <- data_processed %>%
+  filter(include) %>%
+  select(
+    patient_id, elig_date, covid_vax_disease_1_date, death_date, dereg_date, 
+    age_1, jcvi_group, region, sex, imd_Q5, ethnicity
+    ) 
+
+# tidy up
+rm(data_processed)
+
+# summarise eligible data
+data_eligible %>% my_skim(path = file.path(dir_path, "skim_eligible.txt"))
+
+# save data_eligible
 write_rds(
-  data_processed, 
-  file.path(dir_path, "data_processed.rds"),
+  data_eligible,
+  file.path(dir_path, "data_eligible.rds"),
   compress = "gz"
   )
