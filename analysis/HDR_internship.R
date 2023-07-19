5017751ec88b57348a7af8706cd7864c1d7b4491
Femi Olumuyide <99335404+mosaforbes@users.noreply.github.com>
2023-07-19 15:13:35 +0100

    script 1 update

--- analysis/HDR_internship.R
+++ analysis/HDR_internship.R
@@ -1,21 +1,5 @@
-#library(tidyverse)
-#library(here)
-
-# read data_eligible
-#data_eligible <- read_rds(here("output", "extract", "data_eligible.rds"))
-
-# TODO
-# - create variable called covid_vax_disease_1_time which is the days between elig_date and covid_vax_disease_1_date
-# - do the same for death_date and dereg_date (i.e. time since elig_date)
-# - check that there are no negative values for death_time and dereg_time (there shouldn't be, because we've excluded people who died or deregistered before elig_date)
-# - plot the distribution of covid_vax_disease_1_time across different eligibility dates 
-#   (I'd recommend using ggplot2::facet_wrap() to create a separate panel per jcvi_group, and ggplot2::geom_freqpoly() with colour=elig_date to stratify by elig_date within jcvi_group)
-# - count how many patients there are in the following subgroups:
-#   - imd_Q5 x ethnicity
-#   - imd_Q5 x ethnicity x sex
-#   - imd_Q5 x ethnicity x region
-#   - imd_Q5 x ethnicity x jcvi_group
-
+# Set the work directory 
+setwd("C:/Users/Femi/Documents/late-covid-vaccine-uptake")
 
 # Load libraries
 library(arrow)
@@ -107,4 +91,18 @@ table2
 table3 <- data_eligible %>%
   tbl_cross(row = imd_Q5, col = list(ethnicity, region))
 table3
+
+
+# imd_Q5 x ethnicity x jcvi_group
+table4 <- data_eligible %>%
+  tbl_cross(row = imd_Q5, col = list(ethnicity, jcvi_group))
+table4
+
+# Print the tables
+table1
+table2
+table3
+table4
+
+
 
